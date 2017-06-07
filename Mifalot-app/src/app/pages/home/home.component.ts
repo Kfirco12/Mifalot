2bd27ea0c9ac6a623dbeb45ce91290a17e53eea6
dudihakakyan <dudihakakyan@gmail.com>
2017-06-07 14:12:03 +0300

    Separation of users buttons in home page

--- Mifalot-app/src/app/pages/home/home.component.ts
+++ Mifalot-app/src/app/pages/home/home.component.ts
@@ -10,27 +10,31 @@ import { FirebaseListObservable } from 'angularfire2';
 
 export class HomeComponent implements OnInit 
 {
-  private buttons = [
-                      { id: "messages", txt: 'הודעות', icon: 'fa fa-comments fa-2x', color: 'btn-warning'  }, 
-                      { id: "attendance", txt: 'רשימת נוכחות', icon: 'fa fa-pencil-square-o fa-2x', color: 'btn-success' }, 
-                      { id: "pupils-management", txt: 'ניהול חניכים', icon: 'fa fa-user fa-2x', color: 'btn-info' },
-                      { id: "reports", txt: 'דו"חות נוכחות', icon: 'fa fa-bar-chart fa-2x', color: 'btn-primary' }     
-                    ]
+  // Arrays
+  private coachButtons: Array<any>;
+  private managerButtons: Array<any>;
+  private adminButtons: Array<any>;
 
-  private adminButtons = [  
-                            { id: "users-management", txt: 'ניהול משתמשים פעילים', icon: 'fa fa-users fa-2x', color: 'btn-info' }, 
-                            { id: "blocked-users", txt: 'משתמשים חסומים', icon: 'fa fa-user-times fa-2x', color: 'btn-danger' },
-                            { id: "users-confirm", txt: 'משתמשים הממתינים לאישור', icon: 'fa fa-user-plus fa-2x', color: 'btn-success' }    
-                          ]
-
-  private user: FirebaseListObservable<any[]>;
+  // User details object
+  private user: Object;
 
   // ======================
 
   constructor(private afService: AF, private router: Router) 
   { 
-      // Save an observable to user variables in DB
-      this.user = this.afService.af.database.list('registeredUsers/' + this.afService.getUid());
+    this.user = 
+    { 
+      uid: null,
+      name: null,
+      lastName: null,
+      ID: null,
+      permission: null, 
+      phoneNumber: null
+    };
+
+    this.afService.getUserDetails(this.user);
+
+    this.initializeButtons();
   }
 
   // ======================
@@ -43,9 +47,30 @@ export class HomeComponent implements OnInit
 
   // ======================
 
-  ngOnInit() 
+  initializeButtons()
   {
+    this.coachButtons = [
+                          { id: "messages", txt: 'הודעות', icon: 'fa fa-comments fa-2x', color: 'btn-warning'  }, 
+                          { id: "attendance", txt: 'רשימת נוכחות', icon: 'fa fa-pencil-square-o fa-2x', color: 'btn-success' }, 
+                          { id: "pupils-management", txt: 'ניהול חניכים', icon: 'fa fa-user fa-2x', color: 'btn-info' },
+                          { id: "reports", txt: 'דו"חות נוכחות', icon: 'fa fa-bar-chart fa-2x', color: 'btn-primary' }     
+                        ]
+
+    this.managerButtons = [
+                            { id: "messages", txt: 'הודעות', icon: 'fa fa-comments fa-2x', color: 'btn-warning'  }, 
+                            { id: "reports", txt: 'דו"חות נוכחות', icon: 'fa fa-bar-chart fa-2x', color: 'btn-primary' }     
+                          ]
+
+    this.adminButtons = [  
+                          { id: "users-management", txt: 'ניהול משתמשים פעילים', icon: 'fa fa-users fa-2x', color: 'btn-info' }, 
+                          { id: "blocked-users", txt: 'משתמשים חסומים', icon: 'fa fa-user-times fa-2x', color: 'btn-danger' },
+                          { id: "users-confirm", txt: 'משתמשים הממתינים לאישור', icon: 'fa fa-user-plus fa-2x', color: 'btn-success' }    
+                        ]
   }
+
+  // ======================
+
+  ngOnInit() { }
 
   // ======================
   
